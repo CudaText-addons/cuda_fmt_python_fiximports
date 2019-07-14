c131cccf221f58daacbc70ebe1b40d591bd8d0cb
Alexey-t <support@uvviewsoft.com>
2019-07-14 19:51:12 +0300

    change config; fix

--- __init__.py
+++ __init__.py
@@ -1,20 +1,20 @@
-import os
-from cudatext import ed, ini_read
-from cuda_fmt import get_config_filename
+from cudatext import ini_read
+from cuda_fmt import get_config_filename, ed_filename
 from .fiximports import FixImports
+
+fmt = FixImports()
 
 def do_fix_imports(text):
 
     fn = get_config_filename('Python Fix Imports')
-    section = 'op'
-    op_split = ini_read(fn, section, 'split_import_statements', '1')=='1'
-    op_sort = ini_read(fn, section, 'sort_import_statements', '1')=='1'
-    
-    res, textout = FixImports().sortImportGroups(
-        ed.get_filename(), 
+    op_split = ini_read(fn, 'op', 'split_import_statements', '1')=='1'
+    op_sort = ini_read(fn, 'op', 'sort_import_statements', '1')=='1'
+
+    res, textout = fmt.sortImportGroups(
+        ed_filename,
         text,
         splitImportStatements=op_split,
-        sortImportStatements=op_sort)      
+        sortImportStatements=op_sort)
     if res:
         if textout!=text:
             return textout
